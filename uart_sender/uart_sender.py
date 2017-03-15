687322452040916c00c52d9bb434f24ac29cce37
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-03-15 14:25:34 +0530

    Adding sine and square wave generators

--- uart_sender/uart_sender.py
+++ uart_sender/uart_sender.py
@@ -1,8 +1,33 @@
 """This file sends data via UART."""
+import numpy as np
 import serial
 import serial.tools.list_ports
 import sys
 import time
+
+
+def sine(sample_freq, freq, sample):
+    """The wrapper generates a sine wave of given frequency."""
+    if sample == 1:
+        return 1
+    elif sample == 2:
+        return 2
+    value = np.sin(2 * np.pi * freq * sample * (1.0 / sample_freq))
+    return int(min(value * 128 + 128, 255))
+
+
+def square(sample_freq, freq, sample):
+    """The wrapper genereates."""
+    if sample == 1:
+        return 1
+    elif sample == 2:
+        return 2
+    value = np.sign(np.sin(2 * np.pi * freq * sample * (1.0 / sample_freq)))
+    if value > 0:
+        return 255
+    else:
+        return 0
+
 
 all_ports = serial.tools.list_ports.comports()
 usb_port = ""
@@ -15,9 +40,11 @@ if not len(usb_port):
 
 ser = serial.Serial(usb_port)
 
-frequency = input("Enter sampling frequency :- ")
+samp_frequency = input("Enter sampling frequency :- ")
 
-counts = max(int(50000000.0 / frequency) - 4, 1)
+frequency = input("Enter frequency of wave :- ")
+
+counts = max(int(50000000.0 / samp_frequency) - 4, 1)
 
 dummy = raw_input("Ensure S4 is ON and S1 is OFF. Type anything to continue ")
 
@@ -29,7 +56,8 @@ for i in range(4):
 dummy = raw_input("Ensure S4 is OFF and S1 is OFF. Type anything to continue ")
 
 for i in range(0, 8192):
-    ser.write(chr(i % 256))
+    value = sine(samp_frequency, frequency, i)
+    ser.write(chr(value))
 
 time.sleep(5)
 print "Data has been input. Press S1 to view it on LEDs and Headers."
